a6d2b431785969ff02b4633b0dbda68991b91010
maxifischer <maxi.fischer@student.hpi.de>
2019-10-14 17:40:16 +0200

    binned and full train data on 3 eval data with log

--- train.py
+++ train.py
@@ -1,10 +1,18 @@
 from gensim.models import Word2Vec
+import gensim.downloader as api
 import pickle
 from time import time
 import logging
 import os
 
-logging.basicConfig(filename="log1", format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
+logging.basicConfig(filename="./logs/log1", format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
+
+#dataset = api.load("text8")
+#basic_model = Word2Vec(dataset, size=200, window=5, min_count=1, workers=4)
+#print('Time to build vocab: {} mins'.format(round((time() - t) / 60,
+#                                                          2)))
+#basic_model.save('runs/word2vec_further_{}.model'.format("base"))
+
 
 prefixed = [filename for filename in os.listdir('./data/') if filename.startswith("mcc_")]
 print(prefixed)
@@ -28,13 +36,13 @@ for time_bin in prefixed:
     ########### Evaluation
     #print("Vocabulary size: {0}".format(len(model.wv.vocab)))
 
-    analogy_scores = model.wv.evaluate_word_analogies('eval_data/questions-words.txt')
+    model.wv.accuracy('eval_data/questions-words.txt')
     wordsim_clas = model.wv.evaluate_word_pairs('eval_data/wordsim353.tsv')
     wordsim_rel = model.wv.evaluate_word_pairs('eval_data/wordsim_relatedness_goldstandard.txt')
     wordsim_sim = model.wv.evaluate_word_pairs('eval_data/wordsim_similarity_goldstandard.txt')
     #simlex = model.wv.evaluate_word_pairs('eval_data/SimLex-999.txt', dummy4unknown=True)
 
-    print("Analogy scores: {0}".format(analogy_scores.score))
+    #print("Analogy scores: {0}".format(analogy_scores[:-1]))
     print("Wordsim Classic: {0}".format(wordsim_clas))
     print("Wordsim Relatedness: {0}".format(wordsim_rel))
     print("Wordsim Similarity: {0}".format(wordsim_sim))
