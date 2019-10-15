a37f050575a17d77d8f2093715ea81b708cbe97f
maxifischer <maxi.fischer@student.hpi.de>
2019-10-15 11:55:39 +0200

    same experiment, but pre-trained with text8

--- train.py
+++ train.py
@@ -7,11 +7,12 @@ import os
 
 logging.basicConfig(filename="./logs/log1", format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
 
-#dataset = api.load("text8")
-#basic_model = Word2Vec(dataset, size=200, window=5, min_count=1, workers=4)
-#print('Time to build vocab: {} mins'.format(round((time() - t) / 60,
-#                                                          2)))
-#basic_model.save('runs/word2vec_further_{}.model'.format("base"))
+dataset = api.load("text8")
+t = time()
+basic_model = Word2Vec(dataset, size=200, window=5, min_count=1, workers=4)
+print('Time to train: {} mins'.format(round((time() - t) / 60,
+                                                          2)))
+basic_model.save('runs/word2vec_further_{}.model'.format("base"))
 
 
 prefixed = [filename for filename in os.listdir('./data/') if filename.startswith("mcc_")]
@@ -20,18 +21,25 @@ for time_bin in prefixed:
     with open('data/{}'.format(time_bin), 'rb') as f:
         text = pickle.load(f)
     print("Data loaded...")
+    print(time_bin)
 
     ########### EMB per Bucket (EMB-B)
-    t = time()
-    model = Word2Vec(text, size=200, window=5, min_count=1, workers=4)
-    print('Time to build vocab: {} mins'.format(round((time() - t) / 60,
-                                                          2)))
-    model.save('runs/emb_b_{}.model'.format(time_bin))
+    #t = time()
+    #model = Word2Vec(text, size=200, window=5, min_count=1, workers=4)
+    #print('Time to train: {} mins'.format(round((time() - t) / 60,
+    #                                                      2)))
+    #model.save('runs/emb_b_{}.model'.format(time_bin))
     ###########
 
+    ########### EMB per Bucket pretrained
+    t = time()
+    basic_model = Word2Vec.load("runs/word2vec_further_base.model")
+    basic_model.train(text, total_examples=len(text), epochs=basic_model.epochs)
+    print('Time to train: {} mins'.format(round((time() - t) / 60,
+                                                          2)))
+    basic_model.save('runs/emb_b_{}_further.model'.format(time_bin))
 
-
-
+    model = basic_model
 
     ########### Evaluation
     #print("Vocabulary size: {0}".format(len(model.wv.vocab)))
